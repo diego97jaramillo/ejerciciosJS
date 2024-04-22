d912ee9798b925620a7844fdf224f295e993e6aa
diego97jaramillo <diego97jaramillo@gmail.com>
2024-04-21 23:50:27 -0500

    llegamos al ejercicio 30

--- src/js/main.js
+++ src/js/main.js
@@ -77,3 +77,48 @@ console.log(typeof(booleana));
 const arreglo = ['simon', 'freddy', 'alberto']
 
 console.log(arreglo.length);
+// 20.	Declara una variable objeto y asígnale un objeto con al menos tres propiedades. Muestra en consola una de las propiedades del objeto.
+const objeto = {nombre: 'diego', apellido: 'jaramillo', edad: 26 }
+
+console.log(objeto.nombre);
+// 21.	Declara una variable nulo y asígnale el valor null. Muestra en consola su tipo de dato utilizando el operador typeof().
+let nulo = null;
+console.log(typeof(nulo));
+// 22.	Declara una variable indefinido y no le asignes ningún valor. Muestra en consola su tipo de dato utilizando el operador typeof().
+let indefinido
+console.log(typeof(indefinido));
+// 23.	Declara una variable decimal y asígnale un número decimal. Muestra en consola su tipo de dato utilizando el operador typeof().
+let decimal = 1.5;
+console.log(typeof(decimal));
+// 24.	Declara una variable negativo y asígnale un número negativo. Muestra en consola su tipo de dato utilizando el operador typeof().
+let negativo = -5;
+console.log(typeof(negativo));
+// 25.	Utiliza el método indexOf() para encontrar la posición del carácter 'a' en la cadena "Casa" e imprime el resultado en consola.
+let cad = 'casa';
+console.log(cad.indexOf('a'));
+// 26.	Comprueba si la cadena "Javascript" incluye el substring "Script" utilizando el método includes() e imprime el resultado en consola.
+console.log("Javascript".includes('Script'));
+// 27.	Concatena las cadenas "Hola" y "Mundo" utilizando el método concat() y muestra el resultado en consola.
+console.log('Hola'.concat(' Mundo'));
+// 28.	Concatena las cadenas "Hola" y "Mundo" utilizando el operador + y muestra el resultado en consola.
+console.log('Hola' + ' ' + 'Mundo');
+// 29.	Utiliza template strings para concatenar las cadenas "Hola" y "Mundo" e imprime el resultado en consola.
+const cad1 = 'Hola';
+const cad2 = 'Mundo'
+console.log(`${cad1} ${cad2}`);
+// 30.	Declara una variable cadenaConEspacios con el valor " Ejemplo con espacios ". Utiliza el método trimStart() y trimEnd() para eliminar los espacios en blanco al principio y al final, respectivamente, e imprime el resultado en consola.
+let cadenaConEspacios = " Ejemplo con espacios ";
+console.log(cadenaConEspacios.trimEnd() + cadenaConEspacios.trimStart());
+// 31.	Reemplaza la palabra "azul" por "rojo" en la cadena "El cielo es azul" utilizando el método replace() e imprime el resultado en consola.
+// 32.	Utiliza el método slice() para obtener los últimos cuatro caracteres de la cadena "Programación" e imprime el resultado en consola.
+// 33.	Utiliza el método substring() para obtener una subcadena de la cadena "JavaScript" que comience en el índice 2 e imprime el resultado en consola.
+// 34.	Repite la cadena "Hola" tres veces utilizando el método repeat() e imprime el resultado en consola.
+// 35.	Divide la cadena "Esto es una oración de ejemplo" en un array de palabras y muestra el resultado en consola.
+// 36.	Convierte la cadena "Javascript" a mayúsculas utilizando el método toUpperCase() e imprime el resultado en consola.
+// 37.	Convierte la cadena "EJEMPLO" a minúsculas utilizando el método toLowerCase() e imprime el resultado en consola.
+// 38.	Declara una variable numero y asígnale un número. Muestra en consola su tipo de dato utilizando el operador typeof().
+// 39.	Declara una variable booleana y asígnale un valor booleano. Muestra en consola su tipo de dato utilizando el operador typeof().
+// 40.	Declara una variable arreglo y asígnale un array con al menos tres elementos. Muestra en consola la longitud del array.
+// 41.	Declara una variable objeto y asígnale un objeto con al menos tres propiedades. Muestra en consola una de las propiedades del objeto.
+// 42.	Declara una variable nulo y asígnale el valor null. Muestra en consola su tipo de dato utilizando el operador typeof().
+// 43.	Declara una variable indefinido y no le asignes ningún valor. Muestra en consola su tipo de dato utilizando el operador typeof().
